3c1023955ba8eb09e70a1dffd4de09da8c6409e1
Zlatko Hrastic <zlatko.hrastic@syskit.com>
2022-08-01 12:13:24 +0200

    - .net version bump to 472 because of required tls12 version - Projects upgraded to package reference for easier managing

--- SysKit.ODG.App/SysKit.ODG.Office365Service.Tests/GraphHttpProvider/GraphHttpProviderTests.cs
+++ SysKit.ODG.App/SysKit.ODG.Office365Service.Tests/GraphHttpProvider/GraphHttpProviderTests.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using NUnit.Framework;
-using SysKit.ODG.Office365Service.GraphHttpProvider;
 
 namespace SysKit.ODG.Office365Service.Tests.GraphHttpProvider
 {
